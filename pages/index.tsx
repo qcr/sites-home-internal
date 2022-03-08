0b9e7e17986f12c1e1a3dc5494882e58a7044063
Ben Talbot <b.talbot@qut.edu.au>
2022-03-08 18:28:17 +1000

    Add useful links section with basic styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import { styled } from "@mui/material";
+import { Typography, styled } from "@mui/material";
 
 import {
   QcrBody,
   QcrBottomBar,
   QcrFeatureCard,
   QcrPage,
+  QcrTitle,
   QcrTopBar,
 } from "qcr-sites-shared";
 
@@ -18,10 +19,32 @@ const StyledFeatures = styled("div")({
   display: "flex",
   flexDirection: "row",
   flexWrap: "wrap",
+  marginTop: "16px",
 });
 
+const StyledLinks = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  p: {
+    marginTop: "6px",
+    marginBottom: "6px",
+  },
+  a: {
+    fontSize: "1.2rem",
+    textDecoration: "none",
+  },
+  "a:hover": {
+    fontWeight: "bold",
+  },
+});
+
+const StyledTitle = styled(QcrTitle)({
+  fontWeight: "bold",
+  marginBottom: "12px",
+});
+StyledTitle.defaultProps = { variant: "h5", color: "primary" };
+
 export default function HomePage() {
-  console.log(data);
   return (
     <QcrPage>
       <QcrTopBar title="Internal Homepage" />
@@ -37,6 +60,17 @@ export default function HomePage() {
               />
             ))}
         </StyledFeatures>
+        <StyledLinks>
+          <StyledTitle variant="h4" color="primary">
+            Useful links
+          </StyledTitle>
+          {data.links &&
+            data.links.map((l, i) => (
+              <Typography variant="body1">
+                <a href={l.target}>{l.name}</a>
+              </Typography>
+            ))}
+        </StyledLinks>
       </StyledBody>
       <QcrBottomBar />
     </QcrPage>
