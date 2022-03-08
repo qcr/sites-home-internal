3be2e2e33ffc0df0c83ccefd3b7a0b33fecfd061
Ben Talbot <b.talbot@qut.edu.au>
2022-03-08 17:57:45 +1000

    Add feature cards to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,43 @@
-import { QcrBody, QcrBottomBar, QcrPage, QcrTopBar } from "qcr-sites-shared";
+import { styled } from "@mui/material";
 
-import data from "../data/home.yaml";
+import {
+  QcrBody,
+  QcrBottomBar,
+  QcrFeatureCard,
+  QcrPage,
+  QcrTopBar,
+} from "qcr-sites-shared";
+
+import data from "data/home.yaml";
+
+const StyledBody = styled(QcrBody)({
+  maxWidth: "1000px",
+});
+
+const StyledFeatures = styled("div")({
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+});
 
 export default function HomePage() {
   console.log(data);
   return (
     <QcrPage>
       <QcrTopBar title="Internal Homepage" />
-      <QcrBody></QcrBody>
+      <StyledBody>
+        <StyledFeatures>
+          {data.features &&
+            data.features.map((f, i) => (
+              <QcrFeatureCard
+                key={i}
+                text={f.name}
+                linkUrl={f.target}
+                mediaUrls={f.media}
+              />
+            ))}
+        </StyledFeatures>
+      </StyledBody>
       <QcrBottomBar />
     </QcrPage>
   );
