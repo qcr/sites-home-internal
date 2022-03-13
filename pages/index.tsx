96dcf7b5c0a5448d2f567418f997a1d00e876ba2
Ben Talbot <b.talbot@qut.edu.au>
2022-03-14 07:47:16 +1000

    Remove basePath hacks, & make public (closes #3)

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,6 @@ import {
 
 import data from "data/home.yaml";
 
-const BASE_PATH = "/sites-home-internal";
-
 const StyledBody = styled(QcrBody)({
   display: "flex",
   flexDirection: "row",
@@ -67,10 +65,9 @@ export default function HomePage() {
                 linkUrl={f.target}
                 mediaUrls={
                   f.media
-                    ? (typeof f.media === "string"
-                        ? [f.media]
-                        : f.media
-                      ).map((m) => (BASE_PATH ? BASE_PATH + m : m))
+                    ? typeof f.media === "string"
+                      ? [f.media]
+                      : f.media
                     : undefined
                 }
               />
